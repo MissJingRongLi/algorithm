91bb48bb92c108f3724444ea266969c0cbeefbea
LLLi <1962929829@qq.com>
2019-07-11 11:44:29 +0800

    最长子序列升序+降序

--- LIS.js
+++ LIS.js
@@ -1,5 +1,5 @@
 //最长上升子序列
-// 暴力解法
+// 动态规划解法
 function getMax(arr){
 	var dp = new Array(arr.length).fill(1) //存放每一个数字的最长子序列长度
 	var ans = 0 //存放初始值
@@ -29,8 +29,8 @@ function bSearch(num, high, arr){
 		}else{
 			low = mid +1
 		}
-		return low
 	}
+	return low
 }
 function getMax2(arr){
 	var b = []
@@ -49,4 +49,34 @@ function getMax2(arr){
 	}
 	return k
 }
-console.log(getMax2([1,7,3,5,9,4,8]))
+function bSearchMin(num, high, arr){
+	// 更新b[]这个数组，b[i]里面放的是 长度为i的子序列的最后一个数大小
+	var low = 1
+	while(low <= high){
+		var mid = Math.floor((low+high)/2)
+		if(num >= arr[mid]){
+			high = mid -1
+		}else{
+			low = mid +1
+		}
+	}
+	return low
+}
+function getMin(arr){
+	var b = []
+	var k = 1
+	b[1] = arr[0]
+	for(var i = 1; i < arr.length; i++){
+		// 判断数据能放在哪里
+		if(b[k] > arr[i]){
+			// 比当前的最长子序列的末尾值小，就直接追加
+			b[++k] = arr[i]
+		}else{
+			// 更新子序列的相关信息
+			var pos = bSearchMin(arr[i], k, b)
+			b[pos] = arr[i]
+		}
+	}
+	return b
+}
+console.log(getMin([389, 207, 155,300,299, 170, 158, 65]))
