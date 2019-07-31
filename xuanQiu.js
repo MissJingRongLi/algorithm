3fa0bd78998a933a5f216de834980ba3bcde40b9
LLLi <1962929829@qq.com>
2019-07-31 10:44:04 +0800

    集合遍历

--- xuanQiu.js
+++ xuanQiu.js
@@ -4,36 +4,22 @@
  * 
  * ----遍历问题
  */
-function xuanQiu(K, N, num){
-	var res = []
-	var m = 0
-	res[m] = []
-	for(var i = 0; num[i] >= 0; num[i]--){ //每一种小球 依次递减的循环
-		var Tot = N
-		for(var j = i; j < K; j++){ //依据前一个小球个数的变化，影响后面的选择
-			if(num[j] >= Tot && Tot > 0){
-				res[m].push(Tot)
-				Tot = Tot - num[j]
-			}else if(Tot <= 0){
-				res[m].push(0)
-			}else if(num[j] < Tot && j+1 !== K){
-				res[m].push(num[j])
-				Tot = Tot - num[j]
-			}else if(num[j] < Tot && j+1 === K){
-				break
-			}
-			if(j === K-1){
-				m++
-				res[m] = []
-			}
-		}
-		if(num[i] === 0){
-			i++
-		}
-	}
-	console.log(res)
-}
 var K = 3
 var N = 3
 var num = [2,2,3]
-xuanQiu(K, N, num)
+var ans = new Array(15).fill(0)
+function xuanQiu(i, n){
+	if(n === 0){ //总数已经合适
+		console.log(ans.slice(0,K))
+		return
+	}else if( n < 0 || K === i){
+		return
+	}
+	for(var j = 0; j <= num[i]; j++){
+		ans[i] = j
+		// j作为循环的判断条件，说明每一次的选择个数
+		xuanQiu(i+1, n-j)
+	}
+	ans[i] = 0
+}
+xuanQiu(K, N)
